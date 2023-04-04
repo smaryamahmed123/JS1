e7eea42e104aeda74178cd03bcda46245e0bd1ee
smaryamahmed123 <128501483+smaryamahmed123@users.noreply.github.com>
2023-04-04 12:34:21 +0500

    Add files via upload
    Ass 1 JS

---
+++ app.js
@@ -0,0 +1,115 @@
+// alert("thanks for learning js")
+
+// // alert(20 + 20)
+
+
+
+
+
+// var xyz ="abc"; 
+
+// var numb = 26;
+
+// console.log("xyz"); 
+// console.log("numb");
+
+// var a = 30;
+
+// var b = 20;
+  
+// var c = 10;
+
+// var x = 5;
+
+// var y = 2
+
+// var z = (a / b * c + x) - y
+
+// console.log(z)
+
+
+
+
+
+// // var abc ="asd" legal
+
+// // var abc12 ="asd" legal
+
+// // var 567 ="asd" illegal
+
+// // var 34rt ="asd" illegal
+
+// // var !@#$ ="asd" illegal
+
+
+// // var var ="asd" illegal
+
+
+// // var $abc ="asd" legal
+
+// // var $125 ="asd" legal
+
+
+// // var abc 12 ="asd" illegal
+
+
+
+
+
+// var Name = prompt("Name")
+
+// var Email = prompt("Enter your email")
+
+// var password = prompt("Password")
+
+// var city = prompt ("City","Karachi")
+
+// console.log(Name + " " + Email + " " + password + " " + "From" + " " + city)
+
+// alert("Thanks For Filling This Form")
+
+
+
+// var abc ="psq";
+// if (abc == false) {
+//     console.log("asd");
+// }
+
+// // var abc = false;
+
+// else {
+//     console.log("xyz");
+// }
+
+// var age =34;
+
+// if(age = false){
+//     console.log("have a good day");
+// }
+// else{
+//     console.log("have a good dream");
+// }
+
+
+
+
+
+
+
+var a = 10;
+
+var b = a++;
+
+// var c = b + ++a;
+
+console.log("b")
+
+console.log("a")
+
+var a = 10;
+
+var b = 2;
+
+var c = " a + ++b";
+
+console.log("a")
